d63c9f22f62fd6f4410d19119a7ec4bb86a0fce9
Morne Chamberlain <mornechamberlain@sidefx.com>
2023-11-28 12:54:31 -0500

    Bumping to Houdini Version: 20.5.68 Houdini Engine Version: 7.0 (API: 0)

--- Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
@@ -27,12 +27,12 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 20
 #define HAPI_VERSION_HOUDINI_MINOR 5
-#define HAPI_VERSION_HOUDINI_BUILD 67
+#define HAPI_VERSION_HOUDINI_BUILD 68
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
-#define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 6
-#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 2
+#define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 7
+#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 0
 
 // This is a monotonously increasing API version number that can be used
 // to lock against a certain API for compatibility purposes. Basically,
