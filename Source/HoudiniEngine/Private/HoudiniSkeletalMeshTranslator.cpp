5a3ff373506f1ebc1fee75a1a5728c7b75a32d46
Andy Styles <andys@sidefx.com>
2024-04-29 16:32:02 -0400

    Build fixes for 5.4.

--- Source/HoudiniEngine/Private/HoudiniSkeletalMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniSkeletalMeshTranslator.cpp
@@ -1830,8 +1830,6 @@ FHoudiniSkeletalMeshTranslator::CreateSkeletalMeshMaterials(
 				InShapeMeshHGPO.ObjectId, *InShapeMeshHGPO.ObjectName, InShapeMeshHGPO.GeoId, InShapeMeshHGPO.PartId, *InShapeMeshHGPO.PartName, *UnrealMatPerFaceMatNames[FaceIdx]);
 
 			continue;
-
-			MaterialInterface = nullptr;
 		}
 
 		SkeletalMeshImportData::FMaterial CurrentMat;
