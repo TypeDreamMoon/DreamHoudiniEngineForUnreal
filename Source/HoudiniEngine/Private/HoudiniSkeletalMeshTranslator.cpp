a4008beaa7dcafd91f7c57f15ef8750ae2c2fbaf
Andy Styles <andys@sidefx.com>
2024-10-02 18:23:25 -0400

    Reworked Skeleton Mesh matrix conversions so the matrices are consistent on import and export. Added Skeletal Mesh round trip test. Cleanup of Skeletal Mesh code.

--- Source/HoudiniEngine/Private/HoudiniSkeletalMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniSkeletalMeshTranslator.cpp
@@ -1090,7 +1090,7 @@ TArray<FVector> FHoudiniSkeletalMeshTranslator::GetPointForPhysicsBone(const FHo
 {
 	auto Bone = Skeleton.BoneMap.Find(BoneName);
 
-	auto BoneTransform = (*Bone)->UnrealGlobalMatrix.Inverse();
+	auto BoneTransform = (*Bone)->UnrealGlobalTransform.Inverse();
 
 	// Convert Houdini Points to Unreal Points.
 	TArray<FVector> UnrealPoints;
