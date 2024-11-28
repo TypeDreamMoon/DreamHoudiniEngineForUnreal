1bd98c137139df7912937257a1f2eb7e1866f254
Damien Pernuit <damienp@sidefx.com>
2024-11-28 17:02:31 -0500

    - The Save hip / Open hip file commands now properly save the hip file   as hiplc/hipnc when using an Indie/Education license. - Merged PR#330 on behalf of @ChildAdrian   Fix for Decal Material Actor instantiating. - Merged PR #327 on behalf of @promenox   Removed unused reference - this fixes a build error on Linux.

--- Source/HoudiniEngineEditor/Private/HoudiniEngineBakeUtils.cpp
+++ Source/HoudiniEngineEditor/Private/HoudiniEngineBakeUtils.cpp
@@ -2440,7 +2440,7 @@ FHoudiniEngineBakeUtils::BakeInstancerOutputToActors_IAC(
 
 			FTransform CurrentTransform = CurrentInstancedActor->GetTransform();
 
-			AActor* NewActor = FHoudiniInstanceTranslator::SpawnInstanceActor(CurrentTransform, DesiredLevel, InIAC);
+			AActor* NewActor = FHoudiniInstanceTranslator::SpawnInstanceActor(CurrentTransform, DesiredLevel, InIAC, CurrentInstancedActor);
 			if (!IsValid(NewActor))
 			{
 				continue;
