f2e34f0862bb0fd4f3fdf3e383704e71e11eced0
Damien Pernuit <damienp@sidefx.com>
2025-06-06 14:53:03 -0400

    - Added a checkbox to input and node Sync's export options: Export Main Geometry   This lets you decide if you want to send the mesh's main geometry via the input,   and can be useful if, for example, you only want to send the colliders to Houdini.

--- Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
@@ -2261,7 +2261,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForStaticMesh(
 			InInputSettings.bExportLODs,
 			InInputSettings.bExportSockets,
 			InInputSettings.bExportColliders,
-			true,
+			InInputSettings.bExportMainGeometry,
 			bInputNodesCanBeDeleted,
 			InInputSettings.bPreferNaniteFallbackMesh,
 			InInputSettings.bExportMaterialParameters,
@@ -3070,7 +3070,17 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSkeletalMesh(
 	else
 	{
 		bSuccess = FUnrealSkeletalMeshTranslator::HapiCreateInputNodeForSkeletalMesh(
-			SkelMesh, CreatedNodeId, SKName, SKMInputNodeHandle, nullptr, InInputSettings.bExportLODs, InInputSettings.bExportSockets, InInputSettings.bExportColliders, true, bInputNodesCanBeDeleted, InInputSettings.bExportMaterialParameters );
+			SkelMesh, 
+			CreatedNodeId,
+			SKName,
+			SKMInputNodeHandle, 
+			nullptr,
+			InInputSettings.bExportLODs,
+			InInputSettings.bExportSockets,
+			InInputSettings.bExportColliders,
+			InInputSettings.bExportMainGeometry,
+			bInputNodesCanBeDeleted,
+			InInputSettings.bExportMaterialParameters );
 
 		if(!bSuccess)
 		{
@@ -3160,7 +3170,17 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSkeletalMeshComponent(
 	else
 	{
 		bSuccess = FUnrealSkeletalMeshTranslator::HapiCreateInputNodeForSkeletalMesh(
-			SK, CreatedNodeId, SKCName, InputNodeHandle, SKC, InInputSettings.bExportLODs, InInputSettings.bExportSockets, InInputSettings.bExportColliders, true, bInputNodesCanBeDeleted, InInputSettings.bExportMaterialParameters);
+			SK,
+			CreatedNodeId,
+			SKCName,
+			InputNodeHandle,
+			SKC, 
+			InInputSettings.bExportLODs,
+			InInputSettings.bExportSockets,
+			InInputSettings.bExportColliders,
+			InInputSettings.bExportMainGeometry,
+			bInputNodesCanBeDeleted, 
+			InInputSettings.bExportMaterialParameters);
 	}
 
 	// Create/update the node in the input manager
@@ -3602,7 +3622,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForStaticMeshComponent(
 			InInputSettings.bExportLODs, 
 			InInputSettings.bExportSockets, 
 			InInputSettings.bExportColliders,
-			true, 
+			InInputSettings.bExportMainGeometry, 
 			bInputNodesCanBeDeleted, 
 			InInputSettings.bPreferNaniteFallbackMesh,
 			InInputSettings.bExportMaterialParameters,
@@ -3813,7 +3833,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSplineMeshComponents(
 		InInputSettings.bExportLODs, 
 		InInputSettings.bExportSockets, 
 		InInputSettings.bExportColliders,
-		true, 
+		InInputSettings.bExportMainGeometry, 
 		bInputNodesCanBeDeleted, 
 		InInputSettings.bPreferNaniteFallbackMesh,
 		InInputSettings.bExportMaterialParameters,
--- Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
@@ -2064,8 +2064,8 @@ FHoudiniMeshTranslator::CreateStaticMesh_MeshDescription()
 					HGPO.ObjectId, *HGPO.ObjectName, HGPO.GeoId, HGPO.PartId, *HGPO.PartName, SplitId, *SplitGroupName);
 			}
 
-			// If the collider is not visible, stop here
-			if (SplitType == EHoudiniSplitType::InvisibleUCXCollider)
+			// If the collider is not visible, stop here - unless if we dont have mainGeo
+			if (SplitType == EHoudiniSplitType::InvisibleUCXCollider && bHasMainGeo)
 				continue;
 		}
 		else if (SplitType == EHoudiniSplitType::InvisibleSimpleCollider || SplitType == EHoudiniSplitType::RenderedSimpleCollider)
@@ -2083,8 +2083,8 @@ FHoudiniMeshTranslator::CreateStaticMesh_MeshDescription()
 					HGPO.ObjectId, *HGPO.ObjectName, HGPO.GeoId, HGPO.PartId, *HGPO.PartName, SplitId, *SplitGroupName);
 			}
 
-			// If the collider is not visible, stop here
-			if (SplitType == EHoudiniSplitType::InvisibleSimpleCollider)
+			// If the collider is not visible, stop here - unless if we dont have a main geo
+			if (SplitType == EHoudiniSplitType::InvisibleSimpleCollider && bHasMainGeo)
 				continue;
 		}
 
